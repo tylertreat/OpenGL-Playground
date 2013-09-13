5d0463a0ef37996e5825429bb82550d59f6f8890
Tyler Treat <ttreat31@gmail.com>
2013-09-13 03:13:03 -0500

    Implement bilinear interpolation

--- gl_colorchooser/colorchooser.cpp
+++ gl_colorchooser/colorchooser.cpp
@@ -10,7 +10,10 @@
 #include <Angel.h>
 using namespace std;
 
-#define PI 3.14159f
+#define PI			3.14159f
+#define SCALE_X		0
+#define SCALE_Y		1
+#define NO_SCALE	2
 
 const int SCREEN_SIZE = 512;
 const int CIRCLE_SEGMENTS = 48;
@@ -23,6 +26,7 @@ GLuint square_vao;
 GLuint circle_vao;
 GLuint square_shaders;
 GLuint circle_shaders;
+vec4 circle_color(0.0, 1.0, 0.0, 1.0);
 
 // Get the coordinates for a circle with center (cx, cy) and radius r.
 vec2* getCircleVertices(float cx, float cy, float r, int segments) 
@@ -70,6 +74,112 @@ float translateY(int y)
 }
 
 
+float crossProduct(float p1_x, float p1_y, float p2_x, float p2_y,
+				   float p3_x, float p3_y)
+{
+	return (p1_x - p3_x) * (p2_y - p3_y) - (p2_x - p3_x) * (p1_y - p3_y);
+}
+
+
+// Indicates if the point described by (x, y) falls within the triangle described
+// by p1, p2, and p3.
+bool isInTriangle(float x, float y, float p1_x, float p1_y,
+				  float p2_x, float p2_y, float p3_x, float p3_y)
+{
+	bool b1 = crossProduct(x, y, p1_x, p1_y, p2_x, p2_y) < 0.0f;
+	bool b2 = crossProduct(x, y, p2_x, p2_y, p3_x, p3_y) < 0.0f;
+	bool b3 = crossProduct(x, y, p3_x, p3_y, p1_x, p1_y) < 0.0f;
+	return ((b1 == b2) && (b2 == b3));
+}
+
+
+float scaleY(float y)
+{
+	return (1 * (y - BOTTOM_Y)) / (TOP_Y - BOTTOM_Y);
+}
+
+
+float scaleX(float x)
+{
+	return (1 * (x - LEFT_X)) / (RIGHT_X - LEFT_X);
+}
+
+
+// Linearly interpolate in one dimension between the two given colors using
+// the given fraction.
+vec4 interpolate(vec4 c1, vec4 c2, float fraction, int scaleMode)
+{
+	if (scaleMode == SCALE_X)
+	{
+		fraction = scaleX(fraction);
+	}
+	else if (scaleMode == SCALE_Y)
+	{
+		fraction = scaleY(fraction);
+	}
+
+	float r1 = c1.x;
+	float g1 = c1.y;
+	float b1 = c1.z;
+
+	float r2 = c2.x;
+	float g2 = c2.y;
+	float b2 = c2.z;
+
+	float delta_red = r2 - r1;
+	float delta_green = g2 - g1;
+	float delta_blue = b2 - b1;
+
+	float r = r1 + (delta_red * fraction);
+	float g = g1 + (delta_green * fraction);
+	float b = b1 + (delta_blue * fraction);
+
+	r = min(r, 1.0f);
+	r = max(r, 0.0f);
+	g = min(g, 1.0f);
+	g = max(g, 0.0f);
+	b = min(b, 1.0f);
+	b = max(b, 0.0f);
+
+	return vec4(r, g, b, 1.0);
+}
+
+
+// Bilinearly interpolate the color for the given (x, y) coordinate.
+vec4 interpolateColor(float x, float y)
+{
+	vec4 red = vec4(1.0, 0.0, 0.0, 1.0);
+	vec4 green = vec4(0.0, 1.0, 0.0, 1.0);
+	vec4 blue = vec4(0.0, 0.0, 1.0, 1.0);
+	vec4 white = vec4(1.0, 1.0, 1.0, 1.0);
+
+	if (isInTriangle(x, y, RIGHT_X, BOTTOM_Y, RIGHT_X, TOP_Y, LEFT_X, TOP_Y))
+	{
+		// In the upper triangle
+		float delta_y = y - BOTTOM_Y;
+		float delta_x = tanf(45) * delta_y;
+		float diagonal = sinf(45) * delta_x;
+		float horizontal_offset = RIGHT_X - x;
+
+		vec4 v_color = interpolate(red, green, delta_y, SCALE_Y);
+		vec4 d_color = interpolate(red, blue, diagonal, NO_SCALE);
+		return interpolate(v_color, d_color, horizontal_offset, SCALE_X);
+	}
+	else
+	{
+		// In the lower triangle
+		float delta_y = TOP_Y - y;
+		float delta_x = tanf(45) * (TOP_Y - BOTTOM_Y - delta_y);
+		float diagonal = sinf(45) * delta_x;
+		float horizontal_offset = x - LEFT_X;
+
+		vec4 v_color = interpolate(blue, white, delta_y, SCALE_Y);
+		vec4 d_color = interpolate(red, blue, diagonal, NO_SCALE);
+		return interpolate(v_color, d_color, horizontal_offset, SCALE_X);
+	}
+}
+
+
 // Sets up vertex attributes on the GPU
 void init()
 {
@@ -79,11 +189,11 @@ void init()
 	vec2( LEFT_X, TOP_Y ), vec2( LEFT_X, BOTTOM_Y )
   };
 
-  // A color attribute for each corner (red, blue, green, and white)
+  // A color attribute for each corner (red, green, blue, and white)
   vec4 colors[4]  = {
     vec4( 1.0, 0.0, 0.0, 1.0 ),
-	vec4( 0.0, 0.0, 1.0, 1.0 ),
 	vec4( 0.0, 1.0, 0.0, 1.0 ),
+	vec4( 0.0, 0.0, 1.0, 1.0 ),
 	vec4( 1.0, 1.0, 1.0, 1.0 )
   };
 
@@ -146,7 +256,7 @@ void init()
   glEnableVertexAttribArray( vPosition );
 
   GLuint loc = glGetUniformLocation(circle_shaders, "color");
-  glUniform4f(loc, 0.0, 1.0, 0.0, 1.0);
+  glUniform4fv(loc, 1, (GLfloat *) &circle_color);
 
   glBindBuffer( GL_ARRAY_BUFFER, circle_buffer );
   glVertexAttribPointer( vPosition, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0) );
@@ -202,24 +312,25 @@ void
 
 void mouse( int button, int state, int x, int y )
 {
-	float scaled_x = translateX(x);
-	float scaled_y = translateY(y);
-  if ( state == GLUT_DOWN )
-  {
-    switch( button )
+	float translated_x = translateX(x);
+	float translated_y = translateY(y);
+	if ( state == GLUT_DOWN )
 	{
-		case GLUT_LEFT_BUTTON:
-			cout << "mouse down " << scaled_x << ", " << scaled_y << endl;
-			cout << "in bounds " << isInBounds(scaled_x, scaled_y) << endl;
-			if (isInBounds(scaled_x, scaled_y))
-			{
-				// TODO: update circle color
-			}
-			break;
-		case GLUT_MIDDLE_BUTTON: break;
-		case GLUT_RIGHT_BUTTON:  break;
-    }
-  }
+		switch( button )
+		{
+			case GLUT_LEFT_BUTTON:
+				cout << "in bounds " << isInBounds(translated_x, translated_y) << endl;
+				if (isInBounds(translated_x, translated_y))
+				{
+					// Update circle color
+					circle_color = interpolateColor(translated_x, translated_y);
+					init();
+				}
+				break;
+			case GLUT_MIDDLE_BUTTON: break;
+			case GLUT_RIGHT_BUTTON:  break;
+		}
+	}
 }
 
 
