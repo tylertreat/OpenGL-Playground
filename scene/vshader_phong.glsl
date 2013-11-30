18b6e3ea9bf1c9253ab8713372aabd7622ce03e3
Tyler Treat <ttreat31@gmail.com>
2013-11-30 03:52:41 -0600

    Fix phong lighting shader

--- scene/vshader_phong.glsl
+++ scene/vshader_phong.glsl
@@ -8,10 +8,6 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-uniform mat4 prevModel;
-uniform mat4 prevView;
-uniform mat4 prevProjection;
-
 uniform mat3 normalMatrix;
 uniform vec4 lightPosition;
 
@@ -24,9 +20,6 @@ out vec3 fL;
 out vec3 fV;
 out vec2 fTexCoord;
 
-out vec4 pos;
-out vec4 prevPos;
-
 void main() 
 {
   fN = normalMatrix * vNormal;
@@ -34,7 +27,5 @@ void main()
   fV = -(view * model * vPosition).xyz;
   
   fTexCoord = vTexCoord;
-  pos = projection * view * model * vPosition;
-  prevPos = prevProjection * prevView * prevModel * vPosition;
-  gl_Position = pos;
+  gl_Position = projection * view * model * vPosition;
 } 
